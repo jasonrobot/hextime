4e121e14f2e9582f9b212aa1acbc6a982c9d5225
Jason Howell <jasonrobot64@gmail.com>
2019-10-26 13:12:47 -0700

    Made the HexTime thing into a class, all seems to work. Reduced code even more using ramda.

--- src/converter.js
+++ src/converter.js
@@ -6,18 +6,27 @@
  */
 
 import {
+    add,
     // ap,
     // call,
     compose,
+    cond,
     divide,
+    equals,
     flip,
+    head,
     invoker,
+    is,
     juxt,
-    // map,
+    map,
     mathMod,
+    // nth,
     // modulo,
-    // multiply,
+    multiply,
     // prop,
+    reduce,
+    type,
+    zipWith,
 } from 'ramda';
 
 const {
@@ -26,36 +35,57 @@ const {
 
 export const msOfDay = compose(flip(mathMod)(86400000), invoker(0, 'getTime'));
 
-// const mod16 = flip(modulo)(16);
-// const divmod16 = compose(floor, flip(modulo)(16), flip(divide)(16));
-
 /**
  * (conversionFactor : number) -> (Date) -> number
  */
 const hh = factor => compose(
-    floor,
     flip(mathMod)(16),
+    floor,
     flip(divide)(factor),
     msOfDay,
 );
 
-// h1 :: (Date) -> number
-export const h1 = hh(5400000);
-// h2 :: (Date) -> number
-export const h2 = hh(675000 / 2);
-// h3 :: (Date) -> number
-export const h3 = hh(675000 / 32);
-// h4 :: (Date) -> number
-export const h4 = hh(675000 / 512);
-// h5 :: (Date) -> number
-export const h5 = hh(675000 / 8192);
+const conversionFactors = [
+    5400000,
+    675000 / 2,
+    675000 / 32,
+    675000 / 512,
+    675000 / 8192,
+];
 
-// const mht = d => map(flip(call)(d), [h1, h2, h3, h4, h5]);
-// const mht = d => map(f => f(d), [h1, h2, h3, h4, h5]);
-export const makeHexTime = juxt([h1, h2, h3, h4, h5]);
+// makeHexTime :: Date => [number]
+const makeHexTime = juxt(map(hh, conversionFactors));
 
-export const convertHMStoHex = (h, m, s) => makeHexTime(new Date(h, m, s));
+const hexToMillis = compose(
+    reduce(add, 0),
+    zipWith(multiply, conversionFactors),
+);
 
-export const convertHexToHMS = (h1, h2, h3, h4, h5 = 0) => {
-};
+export class HexTime {
+    static fromDateObject(date) {
+        return makeHexTime(date);
+    }
 
+    constructor(...args) {
+        cond([
+            [
+                is(Date, head(args)),
+                this.times = this.constructor.fromDateObject(head(args)),
+            ],
+            [map(equals(type('Number')), args)],
+        ]);
+    }
+
+    /* eslint-disable class-methods-use-this */
+    /**
+     * Use a setter with no effect to prevent setting this.times directly.
+     */
+    // set times(_) {
+    //     throw new Error('Don\'t set times directly.');
+    // }
+    /* eslint-enable class-methods-use-this */
+
+    toDate() {
+        return new Date((new Date()).getTime() + hexToMillis(this.times));
+    }
+}
--- src/converter.test.js
+++ src/converter.test.js
@@ -1,30 +1,51 @@
 import {
-    h1,
-    h2,
+    HexTime,
 } from './converter';
 
 const midnight = new Date('2000-1-1');
 
 describe('converter', () => {
-
     beforeEach(() => {
         midnight.setUTCHours(0);
     });
 
     describe('h1', () => {
         it('should work, right?', () => {
-            expect(h1(midnight)).toEqual(0);
+            const ht = new HexTime(midnight);
+            expect(ht).toBeDefined();
         });
 
         it('should show 12 hours as 7 h1', () => {
             midnight.setUTCHours(12);
-            expect(h1(midnight)).toEqual(8);
+            const ht = new HexTime(midnight);
+            expect(ht.times[0]).toEqual(8);
         });
     });
 
     describe('h2', () => {
         it('should show midnight as 0 h2', () => {
-            expect(h2(midnight)).toEqual(0);
+            midnight.setUTCHours(1);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(2);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(3);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(4);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(5);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(6);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(7);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(8);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(9);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(10);
+            console.log((new HexTime(midnight)).times);
+            midnight.setUTCHours(11);
+            console.log((new HexTime(midnight)).times);
         });
     });
 });
